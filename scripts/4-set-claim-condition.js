141c2e328776db85c5d766fe6543be23378d0069
HiggsBosonic <natstonecipher@gmail.com>
2021-12-26 09:51:17 -0500

    claim conditions and NFT deploy

---
+++ scripts/4-set-claim-condition.js
@@ -0,0 +1,23 @@
+import sdk from "./1-initialize-sdk.js";
+
+const bundleDrop = sdk.getBundleDropModule(
+  "0x43D7090688D29335f6c8fB47310b87b93De74992",
+);
+
+(async () => {
+  try {
+    const claimConditionFactory = bundleDrop.getClaimConditionFactory();
+    // Specify conditions.
+    claimConditionFactory.newClaimPhase({
+      startTime: new Date(),
+      maxQuantity: 50_000,
+      maxQuantityPerTransaction: 1,
+    });
+    
+    
+    await bundleDrop.setClaimCondition(0, claimConditionFactory);
+    console.log("✅ Sucessfully set claim condition on bundle drop:", bundleDrop.address);
+  } catch (error) {
+    console.error("Failed to set claim condition", error);
+  }
+})()
