9a814b8f021cde57d821ec0bc07e7d6ea2582b2c
HiggsBosonic <natstonecipher@gmail.com>
2021-12-26 09:15:12 -0500

    ducks ducks ducks ducks ducks

--- scripts/2-deploy-drop.js
+++ scripts/2-deploy-drop.js
@@ -12,7 +12,7 @@ const app = sdk.getAppModule("INSERT_YOUR_APP_ADDRESS_HERE");
       // A description for the collection.
       description: "Getting Families into DAO and web3.",
       // The image for the collection that will show up on OpenSea.
-      image: readFileSync("scripts/assets/naruto.png"),
+      image: readFileSync("scripts/assets/ducks.jpeg"),
       // We need to pass in the address of the person who will be receiving the proceeds from sales of nfts in the module.
       // We're planning on not charging people for the drop, so we'll pass in the 0x0 address
       // you can set this to your own wallet address if you want to charge for the drop.
